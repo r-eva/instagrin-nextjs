ec2c9b45c996b9d63be8d30ac5161c62f412bd0c
Regina Eva <bildung@reginaeva.de>
2024-04-13 11:41:32 +0200

    basic json file created

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import AuthForm from "@/components/auth/auth-form";
-export default function Home() {
+import getUser from "@/app/helper/action";
+export default async function Home() {
+  const user = await getUser();
+  console.log(user);
   return <AuthForm />;
 }
