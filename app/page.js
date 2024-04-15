b6dff9c9c513b6480e75eb497b301b55338765c6
ccandradewi <cornelia.candradewi@outlook.com>
2024-04-15 13:36:49 +0700

    already finish suggested component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import AuthForm from "@/components/auth/auth-form";
 import getUser from "@/app/helper/action";
+import StartingPageContent from "@/components/starting-page/starting-page";
+import Post from "@/components/post/post";
 export default async function Home() {
   const user = await getUser();
-  console.log(user);
+  // console.log(user);
   // return <AuthForm />;
-  return<StartingPageContent/>
+  return(
+    <>
+    <StartingPageContent userData={user}/>
+    </>
+  )
 }
--- components/starting-page/starting-page.js
+++ components/starting-page/starting-page.js
@@ -2,13 +2,13 @@ import Sidebar from "../sidebar/sidebar";
 import Suggested from "../suggested/suggested";
 import classes from "./starting-page.module.css";
 
-function StartingPageContent() {
+function StartingPageContent({userData}) {
   // Show Link to Login page if NOT auth
-
+console.log('isi userdata', userData)
   return (
     <section className={classes.starting}>
       <Sidebar />
-      <Suggested/>
+      <Suggested data={userData}/>
     </section>
   );
 }
