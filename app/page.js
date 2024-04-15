5a580b7dd351d037efd85e15651edabf8327bb4b
ccandradewi <cornelia.candradewi@outlook.com>
2024-04-15 23:51:09 +0700

    make base skeleton post for homepage

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import AuthForm from "@/components/auth/auth-form";
 import getUser from "@/app/helper/action";
+import Post from "@/components/post/post";
 export default async function Home() {
   const user = await getUser();
   console.log(user);
-  return <AuthForm />;
+  // return <AuthForm />;
+  return <Post/>
 }
