46587a83a5bbd23325bbd80128af8a57848f26bc
ccandradewi <cornelia.candradewi@outlook.com>
2024-04-18 12:06:28 +0700

    add map image for post section

--- components/profile-page/profile-page.js
+++ components/profile-page/profile-page.js
@@ -1,5 +1,6 @@
 import Sidebar from "../sidebar/sidebar";
-import {Cog6ToothIcon} from '@heroicons/react/24/outline'
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+import { ImagePost } from "./imagepost";
 function ProfilePage() {
   return (
     <div className="flex flex-row">
@@ -14,10 +15,16 @@ function ProfilePage() {
             ></img>
           </div>
           <div className=" col-span-2">
-            <span className=" text-gray-700 text-2xl mr-3">cornelia_mustika</span>
-            <div className=" cursor-pointer inline text-sm text-gray-700 font-semibold p-1 px-2 border border-gray-200 rounded mr-3">Edit Profile</div>
-            <div className=" cursor-pointer inline text-sm text-gray-700 font-semibold p-1 px-2 border border-gray-200 rounded mr-3">View Archive</div>
-            <Cog6ToothIcon className=" cursor-pointer h-6 inline text-black"/>
+            <span className=" text-gray-700 text-2xl mr-3">
+              cornelia_mustika
+            </span>
+            <div className=" cursor-pointer inline text-sm text-gray-700 font-semibold p-1 px-2 border border-gray-200 rounded mr-3">
+              Edit Profile
+            </div>
+            <div className=" cursor-pointer inline text-sm text-gray-700 font-semibold p-1 px-2 border border-gray-200 rounded mr-3">
+              View Archive
+            </div>
+            <Cog6ToothIcon className=" cursor-pointer h-6 inline text-black" />
             <div className=" mt-4 flex text-black">
               <div className=" font-semibold">20 post</div>
               <div className=" ml-4 font-semibold"> 200 followers</div>
@@ -25,34 +32,30 @@ function ProfilePage() {
             </div>
           </div>
         </div>
-        <hr className=" border-gray-500 mt-6"/>
-      <div className=" flex justify-center gap-10 -mt-[0.5px]">
-        <button className=" focus:border-t border-gray-800 py-8 text-sm font-semibold focus:text-gray-600">
-          POST
-        </button>
-        <button className="focus:border-t border-gray-800 py-8 text-sm font-semibold focus:text-gray-600">
-          REELS
-        </button>
-        <button className="focus:border-t border-gray-800 py-8 text-sm font-semibold focus:text-gray-600">
-          SAVED
-        </button>
-        <button className="focus:border-t border-gray-800 py-8 text-sm font-semibold focus:text-gray-600">
-          TAGGED
-        </button>
-      </div>
-      <div className=" grid grid-cols-3 gap-5">
-        <div className=" overflow-hidden h-64 object-cover">
-          <div>
-          <img src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg" alt="post 1"/>
-          </div>
+        <hr className=" border-gray-500 mt-6" />
+        <div className=" flex justify-center gap-10 -mt-[0.5px]">
+          <button className=" focus:border-t border-gray-800 py-8 text-sm font-semibold focus:text-gray-600">
+            POST
+          </button>
+          <button className="focus:border-t border-gray-800 py-8 text-sm font-semibold focus:text-gray-600">
+            REELS
+          </button>
+          <button className="focus:border-t border-gray-800 py-8 text-sm font-semibold focus:text-gray-600">
+            SAVED
+          </button>
+          <button className="focus:border-t border-gray-800 py-8 text-sm font-semibold focus:text-gray-600">
+            TAGGED
+          </button>
         </div>
-        <div>
-        <img src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg" alt="post 2"/>
+        <div className=" grid grid-cols-3 gap-5">
+          {ImagePost.map((photo, index)=>(
+            <div className="h-64 overflow-hidden" key={index}>
+              <div className="relative h-64 ">
+              <img className=" object-cover h-64" src={photo.image}></img>
+              </div>
+            </div>
+          ))}
         </div>
-        <div>
-        <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_640.jpg" alt="post 1"/>
-        </div>
-      </div>
       </div>
     </div>
   );
